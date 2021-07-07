3b66b9f7c175af061b33ea303d2928d1addd120b
Craig Sorensen <craigs@uoregon.edu>
2021-07-06 17:39:10 -0700

    add better temp logic

---
+++ app2.py
@@ -0,0 +1,130 @@
+import os
+import logging
+
+from datetime import datetime
+
+from weather_api import weather
+from indoor_temp import get_indoor_temperature
+from send_push import push
+from dbman import db
+
+CRED_DIR = os.path.expanduser("~")
+api_key_file = "{0}/.openweatherapi.txt".format(CRED_DIR)
+push_api_cred_file = "{0}/.openwindows_push_api.txt".format(CRED_DIR)
+SCRIPT_EXC_DIR = os.path.dirname(os.path.realpath(__file__))
+db_path = "{0}/tempdb.json".format(SCRIPT_EXC_DIR)
+log_dir = f'{SCRIPT_EXC_DIR}/app.log'
+LOCAL_ZIPCODE = "97477,us"
+date = datetime.now()
+
+
+
+# Setup logging
+logging.basicConfig(filename=log_dir, format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
+
+# Get weather API credentials from disk
+if os.path.isfile(api_key_file):
+    with open(api_key_file, 'r') as f:
+        WEATHER_API_KEY = f.readline().strip()
+else:
+    print(f"No API Credential file found. Expecting: {api_key_file} - Check README file for setup instuctions")
+    logging.info(f"No API Credential file found. Expecting: {api_key_file} - Check README file for setup instuctions")
+if not WEATHER_API_KEY:
+    print(f"No API Credentials found in: {api_key_file} - Check README file for setup instuctions")
+    logging.info(f"No API Credentials found in: {api_key_file} - Check README file for setup instuctions")
+
+# Get push API credentials from disk
+if os.path.isfile(push_api_cred_file):
+    with open(push_api_cred_file, 'r') as f:
+        creds = f.readlines()
+    TOKEN = creds[0].strip().split(':')[1]
+    USER = creds[1].strip().split(':')[1]
+else:
+    print(f"No API Credentials found in: {push_api_cred_file} - Check README file for setup instuctions")
+    logging.info(f"No API Credentials found in: {push_api_cred_file} - Check README file for setup instuctions")
+
+
+
+indoor_temp = round(float(get_indoor_temperature()['temperature']))
+outdoor_temp = int(round((weather.WeatherMan(WEATHER_API_KEY, LOCAL_ZIPCODE)).temperature))
+
+hour = int(date.strftime("%H"))
+DEGREE_BUFFER = 2
+message = f"Inside: {indoor_temp} || Outside: {outdoor_temp} || Outside Adjusted: {outdoor_temp - DEGREE_BUFFER}"
+
+def get_time_boundary(h):
+    '''
+    Calculates if operation is happening in the in the morning or evening and returns if the windows should be open or closed.
+    '''
+    if h >= 8 and h <= 4:
+        return "close"
+    if h >= 17 and h <= 23:
+        return "open"
+    return "OOB"
+
+def get_notification_lock_status(db):
+    # manage the lockfile. Update it, or delete it if it's old.
+
+    if db["notification_sent"]:
+        print("Notification has already been sent.")
+        logging.info("Notification has already been sent.")
+        quit()
+    else:
+        return False
+
+
+boundary = get_time_boundary(hour)
+tempdb = db.dbman(db_path)
+
+# Check if db tempratures need to be updated, if so update them.
+if indoor_temp > tempdb['indoor_max_temp']:
+    print('Indoor temp is higher than temp, in db. updating record.')
+    tempdb['indoor_max_temp'] = indoor_temp
+    dbman.write_database_to_disk(tempdb)
+    tempdb = dbman.get_db()
+if outdoor_temp > tempdb['outdoor_max_temp']:
+    print('Outdoor temp is higher than temp, in db. updating record.')
+    tempdb['outdoor_max_temp'] = outdoor_temp
+    dbman.write_database_to_disk(tempdb)
+    tempdb = dbman.get_db()
+
+if boundary == "close":
+    get_notification_lock_status(tempdb)
+    if (outdoor_temp - DEGREE_BUFFER) >= indoor_temp:
+        # close windows
+        print(f"CLOSE WINDOWS! {message}")
+        push.send(TOKEN, USER, f"CLOSE WINDOWS! {message}")
+        print("Sent Close windows notification")
+        logging.debug(f"Writing lock file - date: {date.strftime('%b %d, %Y')} status: {boundary}")
+        tempdb['notification_sent'] = True
+        db.dbman.write_database_to_disk()
+    else:
+        #log temp and do nothing
+        print(message)
+        print("It's colder outside, recommend doing nothing!")
+        logging.info(message)
+        logging.info("It's colder outside, recommend doing nothing!")
+
+
+if boundary == "open":
+    manage_lockfile(lock_file_location)
+    if (outdoor_temp - DEGREE_BUFFER) <= indoor_temp:
+        print(message)
+        push.send(TOKEN, USER, f"OPEN WINDOWS {message}")
+        print("Sent OPEN windows notification")
+        logging.debug(f"Writing lock file - date: {date.strftime('%b %d, %Y')} status: {boundary}")
+
+        with open(lock_file_location, 'w') as f:
+            f.write(date.strftime("%b %d, %Y")+"\n")
+            f.write(f"{boundary}\n")
+
+    else:
+        #log temp and do nothing
+        print(message)
+        print("It's WARMER outside, recommend doing nothing!")
+        logging.info(message)
+        logging.info("It's WARMER outside, recommend doing nothing!")
+
+if boundary == "OOB":
+        print("Not in operational boundary.")
+        logging.info(f"OOB: {message}")
---
+++ dbman/db.py
@@ -0,0 +1,80 @@
+import json
+import os
+
+from datetime import datetime
+# get temps
+# check for max_temp_file, if not found, create.
+# get previous max temps
+# if current outside temp is higher than previous, update max_temp_file
+# if current inside temp is higher than previous, update max_temp_file
+# if outside temp is higher than highest inside temp + buffer, set send notification to true
+
+#variables
+indoor_temp = 71
+outdoor_temp = 81
+temp_buffer = 5
+
+
+date = datetime.now() # get current date/time
+pretty_date = date.strftime("%b-%d-%Y") #Nov-01-2021
+
+class db_manager():
+    def __init__(self, tempurature_db_location):
+        #file locations
+        self.db_location = tempurature_db_location
+        self.date = datetime.now() # get current date/time
+        self.pretty_date = self.date.strftime("%b-%d-%Y") #Nov-01-2021
+
+    def create_blank_db(self):
+        db = {}
+        db["indoor_max_temp"] = 0
+        db["outdoor_max_temp"] = 0
+        db["db_creation_date"] = self.pretty_date
+        db["send_notifcation"] = False
+        db["notification_sent"] = False
+        return db
+
+    def get_db(self):
+        with open(self.db_location, 'r') as f:
+            db = json.load(f)
+            return db
+
+    def write_database_to_disk(self, data):
+        with open(self.db_location, 'w') as f:
+            json.dump(data, f)
+
+    def check_if_db_file_exists(self):
+        if os.path.isfile(self.db_location):
+            print(f"db found in: {self.db_location}")
+            return True
+        # nope, return false.
+        print(f"db not found in: {self.db_location}")
+        return False
+
+## Main ##
+
+dbman = db_manager(temp_db_location)
+
+print("Checking if tempdb exists..")
+if(dbman.check_if_db_file_exists()):
+    tempdb = dbman.get_db()
+    # check if tempdb was created today, if not purge the data and start fresh.
+    print(f"db creation date: {tempdb['db_creation_date']}")
+else:
+    print("No database found, creating!")
+    tempdb = dbman.create_blank_db()
+
+
+if tempdb['db_creation_date'] == pretty_date:
+    print("database was created today.. lets use it")
+else:
+    print("old database data found. Purge!")
+    tempdb = dbman.create_blank_db()
+    dbman.write_database_to_disk(tempdb)
+    print("loading new db from disk")
+    tempdb = dbman.get_db()
+
+
+
+# Check if notification should be sent
+# Current process is if outside temp is was ever 10 degrees warmer than inside.
