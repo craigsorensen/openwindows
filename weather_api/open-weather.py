c62f42afc2a0d9f8e023681800964baf85f1a863
Craig Sorensen <draker@draker.net>
2022-07-13 22:40:32 -0700

    update to use weatherapi.com

--- weather_api/open-weather.py
+++ weather_api/open-weather.py
@@ -1,4 +1,4 @@
-# Get weather from weatherapi.com
+# Get weather from openweathermap api
   
 # import required modules 
 import requests
@@ -14,36 +14,36 @@ class WeatherMan:
         self.key = key
         self.zipcode = zipcode
         self.weather = self._get_weather
-        
 
     @property
     def _get_weather(self):
+
         # base_url variable to store url 
-        BASE_URL = "http://api.weatherapi.com/v1/current.json?"
+        BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 
         # complete_url variable to store 
-        complete_url = f'{BASE_URL}key={self.key}&q={self.zipcode}&aqi=no'
+
+        complete_url = BASE_URL + "appid=" + self.key + "&q=" + self.zipcode + "&units=imperial"
         logging.debug(f"Weather URL: {complete_url}")
-        #print(f'URI: {complete_url}')
         response = requests.get(complete_url) 
         r = response.json() 
         logging.debug(f"Weather API response: {r}")
-        #print(f"Weather API response: {r}")
 
-        if response.status_code == 200: 
+        if r["cod"] == 200: 
             
-            c = r["current"] 
-            current_temperature = c["temp_f"] 
-            current_humidiy = c["humidity"]     
+            m = r["main"] 
+            current_temperature = m["temp"] 
+            current_humidiy = m["humidity"]     
+            w = r["weather"]
 
-            #print(f"Temperature: {str(current_temperature)}F") 
-            #print(f"Humidity: {str(current_humidiy)}%") 
-        elif response.status_code == 404:
+            # print(f"Temperature: {str(current_temperature)}F") 
+            # print(f"Humidity: {str(current_humidiy)}%") 
+        elif r["cod"] == 404:
             print(f"City Not Found for: {zipcode}")
             logging.info(f"City Not Found for: {zipcode}")
         else:
-            print(f"Error {response.status_code}")
-            logging.info(f"Error {response.status_code}")
+            print(f"Error {r['cod']} - {r['message']}")
+            logging.info(f"Error {r['cod']} - {r['message']}")
             exit()
         
         return { "tempurature": current_temperature, "humidity": current_humidiy }
